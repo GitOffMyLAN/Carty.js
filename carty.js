07213a81f77ce4322f116a2ceef770800feaddbc
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-29 17:06:52 +1000

    Update carty.js

--- carty.js
+++ carty.js
@@ -4,4 +4,16 @@ class cart {
     this.links = links;
     this.names = names;
   }
+  //get url
+  get getURL(){
+    return url;
+  }
+  //get links
+  get getLinks(){
+    return links;
+  }
+  //get names
+  get getNames(){
+    return names;
+  }
 }
